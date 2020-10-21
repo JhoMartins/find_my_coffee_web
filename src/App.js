fb4700826b4567f58257101794a39f0530f60efb
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-10-21 20:23:44 -0300

    get user current location in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { GoogleMap , LoadScript } from '@react-google-maps/api';
 
 function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+
   const { REACT_APP_GOOGLE_KEY } = process.env
+
+  useEffect(() => {
+    setCurrentLocation()
+  }, [])
+
+  async function setCurrentLocation() {
+    await navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+
+    }, function(error) {
+      alert("Habilite a localização para usar esse APP")
+    }
+    )
+  }
 
   return (
     <Fragment>
       <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_KEY}>
-        <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}} zoom={15} center={{lat: -22.773982, lng: -47.4160998}}>
+        <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}} zoom={15} center={{lat: latitude, lng: longitude}}>
 
         </GoogleMap>
       </LoadScript>
