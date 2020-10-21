d474a71f81176867575554c915c493d280c7bbbe
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-10-21 20:16:10 -0300

    add google map in screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react';
+import { GoogleMap , LoadScript } from '@react-google-maps/api';
 
 function App() {
+  const { REACT_APP_GOOGLE_KEY } = process.env
+
   return (
     <Fragment>
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_KEY}>
+        <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}} zoom={15} center={{lat: -22.773982, lng: -47.4160998}}>
 
+        </GoogleMap>
+      </LoadScript>
     </Fragment>
   );
 }
